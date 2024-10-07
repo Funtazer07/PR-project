48c1f5ff4191907656fffc88ee781bcae8e3ab81
Andrejs <a.dvoskins@student.fontys.nl>
2024-10-07 15:16:31 +0200

    first type of form submission and improved css

--- reserve/form.php
+++ reserve/form.php
@@ -2,11 +2,11 @@
 // Получаем данные из полей ввода
 $name = $_POST['name'];
 $email = $_POST['email'];
-$comment = $_POST['comment'];
+$phone = $_POST['phone'];
 $to = 'andvoskin@gmail.com';
-$header = 'Отзыв от пользователя'
+$header = 'Pietiekšanas forma'
 // Собираем все данные в письмо
-$message = "Имя пользователя: $name \nЭлектронная почта: $email \nОтзыв: $comment";
+$message = "Имя пользователя: $name \nЭлектронная почта: $email \nОтзыв: $phone";
 // Отправляем письмо
 $send = mail($to, $header, $message, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
 ?>
