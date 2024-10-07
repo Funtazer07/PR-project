9e386fb3fa3d897b045492f9f398af05390247e3
Andrejs <a.dvoskins@student.fontys.nl>
2024-10-07 15:43:52 +0200

    add js

---
+++ reserve/script.js
@@ -0,0 +1,7 @@
+document.getElementById("pietiekties").addEventListener("click", function() {
+    // Select the target element you want to scroll to
+    const targetElement = document.getElementById("submit");
+
+    // Scroll smoothly to the target element
+    targetElement.scrollIntoView({ behavior: "smooth" });
+});
