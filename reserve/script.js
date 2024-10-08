d5d849a5c6f8bc0c1b42ec0d1bb19a9895571427
Andrejs <a.dvoskins@student.fontys.nl>
2024-10-08 12:09:25 +0200

    add resposnive

--- reserve/script.js
+++ reserve/script.js
@@ -5,3 +5,68 @@ document.getElementById("pietiekties").addEventListener("click", function() {
     // Scroll smoothly to the target element
     targetElement.scrollIntoView({ behavior: "smooth" });
 });
+
+
+// Get the modal
+var modal = document.getElementById("modal");
+
+// Get the modal image and caption
+var modalImg = document.getElementById("modal-img");
+var captionText = document.getElementById("caption");
+
+// Get all the images in the gallery
+var images = document.querySelectorAll('.gallery-img');
+
+// Track the index of the current image
+var currentIndex;
+
+// Function to check if the screen width is less than 480px
+function isSmallScreen() {
+    return window.innerWidth <= 480;
+}
+
+// Add click event listener to each image
+images.forEach((img, index) => {
+    img.addEventListener('click', function() {
+        if (!isSmallScreen()) {
+            // Only open the modal if the screen is larger than 480px
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+            currentIndex = index; // Set the current index for navigation
+        }
+    });
+});
+
+// Close the modal when the close button is clicked
+var closeBtn = document.getElementsByClassName("close")[0];
+closeBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Navigation: move to previous image
+var prev = document.querySelector('.prev');
+prev.onclick = function() {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    updateModalImage();
+}
+
+// Navigation: move to next image
+var next = document.querySelector('.next');
+next.onclick = function() {
+    currentIndex = (currentIndex + 1) % images.length;
+    updateModalImage();
+}
+
+// Update the modal with the new image and caption
+function updateModalImage() {
+    modalImg.src = images[currentIndex].src;
+    captionText.innerHTML = images[currentIndex].alt;
+}
+
+// Disable slideshow and modal functionality for small screens
+window.addEventListener('resize', function() {
+    if (isSmallScreen()) {
+        modal.style.display = "none"; // Ensure modal closes if screen is resized to small
+    }
+});
