09fa10feeb61cc4c50884aebf8cbaa61a86a1ce4
Andrejs <a.dvoskins@student.fontys.nl>
2024-10-09 13:03:25 +0200

    get rid of necessary comments and format files

--- exchange/script.js
+++ exchange/script.js
@@ -15,25 +15,6 @@ document.getElementById("uzzinat").addEventListener("click", function() {
 });
 
 
-
-// Get the modal
-var modal = document.getElementById("modal");
-
-// Get the modal image and caption
-var modalImg = document.getElementById("modal-img");
-var captionText = document.getElementById("caption");
-
-// Get all the images in the gallery
-var images = document.querySelectorAll('.gallery-img');
-
-// Track the index of the current image
-var currentIndex;
-
-// Function to check if the screen width is less than 480px
-function isSmallScreen() {
-    return window.innerWidth <= 480;
-}
-
 let slideIndex = 0;
 showSlides(slideIndex);
 
@@ -59,36 +40,3 @@ function showSlides(n) {
     // Show the active image
     slides[slideIndex].classList.add('active');
 }
-
-
-
-function updateOptions() {
-    // Get the selected number of rooms from the dropdown
-    var roomsSelect = document.getElementById("roomsSelect").value;
-
-    // Get the dropdown element for area options
-    var optionsSelect = document.getElementById("optionsSelect");
-
-    // Clear the current options in the area dropdown
-    optionsSelect.innerHTML = "";
-
-    // Define area options based on the selected number of rooms
-    var options = [];
-
-    if (roomsSelect === "2") {
-        options = ["61 m2","66 m2"];
-    } else if (roomsSelect === "3") {
-        options = ["61 m2","66 m2"];
-    } else {
-        options = ["Nav pieejamas opcijas"];
-    }
-
-    // Populate the area dropdown with the new options
-    options.forEach(function(option) {
-        var opt = document.createElement("option");
-        opt.value = option;
-        opt.innerHTML = option;
-        optionsSelect.appendChild(opt);
-    });
-}
-
